3b32ea4f2834f9bd9c5c26093d09630105906a49
Homura <esy.minmax@gmail.com>
2024-05-30 23:57:50 +0800

    Working through rest documentation

---
+++ src/main/java/com/grouppy/spring/test.java
@@ -0,0 +1,44 @@
+//package com.grouppy.spring;
+//import java.sql.*;
+//
+//public class test {
+//    //Java program to set up connection and get all data from table
+//
+//        public static void main(String arg[])
+//        {
+//            Connection connection = null;
+//            try {
+//                // below two lines are used for connectivity.
+//                Class.forName("com.mysql.cj.jdbc.Driver");
+//                connection = DriverManager.getConnection(
+//                        "jdbc:mysql://localhost:3306/grouppy",
+//                        "homura", "homura");
+//
+//                // mydb is database
+//                // mydbuser is name of database
+//                // mydbuser is password of database
+//
+//                Statement statement;
+//                statement = connection.createStatement();
+//                ResultSet resultSet;
+//                resultSet = statement.executeQuery(
+//                        "select * from users");
+//                int code;
+//                String title;
+//                while (resultSet.next()) {
+//                    code = resultSet.getInt("id");
+//                    title = resultSet.getString("name").trim();
+//                    System.out.println("Code : " + code
+//                            + " Title : " + title);
+//                }
+//                resultSet.close();
+//                statement.close();
+//                connection.close();
+//            }
+//            catch (Exception exception) {
+//                System.out.println(exception);
+//            }
+//        } // function ends
+//    } // class ends
+//
+//
