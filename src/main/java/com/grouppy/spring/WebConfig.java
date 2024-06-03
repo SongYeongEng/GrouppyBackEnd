e0d13ff7dd583a0cbfbf7433d05bfebfe79c2a55
Homura <esy.minmax@gmail.com>
2024-06-03 15:48:37 +0800

    adding config for Cross-Origin Resource Sharing

---
+++ src/main/java/com/grouppy/spring/WebConfig.java
@@ -0,0 +1,17 @@
+package com.grouppy.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
